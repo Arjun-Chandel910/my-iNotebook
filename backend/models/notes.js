e61cae07f99dceee038f97bc3e3e94ba478f891e
Arjun Chandel <arjunchandel910@gmail.com>
2025-02-01 17:15:06 +0530

    added notes and user notes relation

--- backend/models/notes.js
+++ backend/models/notes.js
@@ -1,11 +1,17 @@
 const mongoose = require("mongoose");
 const NotesSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
   title: {
     type: String,
+    min: 3,
     required: true,
   },
   description: {
     type: String,
+    min: 5,
     required: true,
   },
   tag: {
--- backend/router/notes.js
+++ backend/router/notes.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const router = express.Router();
+const fetchUser = require("../middleware/islogged");
+const Notes = require("../models/notes");
 
-router.get("/", (req, res) => {
-  res.json([]);
+router.get("/getallnotes", fetchUser, async (req, res) => {
+  const notes = await Notes.find({ user: req.user.id });
+  res.json({ notes });
+});
+
+router.post("/addnote", fetchUser, async (req, res) => {
+  let userId = req.user.id;
+  let { title, description } = req.body;
+  let note = new Notes({ user: userId, title, description });
+  await note.save();
+  res.json({ note });
 });
 
 module.exports = router;
